7b4f5b119633bbf35a9c21eaf9fdb20303aec27b
Nicolo Massari <nmassari@student.ethz.ch>
2025-03-30 20:48:16 +0200

    Maintain dive script with direct thrust control

--- maintain_dive.py
+++ maintain_dive.py
@@ -1,45 +1,55 @@
-import rclpy
 from rclpy.node import Node
-from geometry_msgs.msg import PointStamped
+from rclpy import shutdown, init, spin 
+from sensor_msgs.msg import FluidPressure
 import subprocess
 
 glider = "glider_nautilus"
 
-class BladderMonitor(Node):
+# CONSTANTS
+
+# p=p0+k*z
+__std_pressure__ = 101.325  # kPa
+__k__ = 9.80638 # kPa/m
+
+class BadderControlUnit(Node):
     def __init__(self):
         super().__init__('bladder_monitor')
 
         # Inputs
-        self.target_depth = 1.
-        self.isStrokeDownward = True
+        self.target_depth = 4
+        self.is_stroke_downward = True
 
-        # Parameters
+        # Bladder Parameters
         self.bladder_topic = f"/model/{glider}/joint/bladder_joint/cmd_thrust"
         self.neutral_thrust = -35.75  # Known neutral thrust
-        self.down_thrust = -10  # Minimum thrust differential (downwards)
-        self.up_thrust = +10  # Maximum thrust differential (upwards)
-        self.has_reached_target_depth = False
+        self.down_thrust = -25  # Minimum thrust differential (downwards)
+        self.up_thrust = +25  # Maximum thrust differential (upwards)
+
+        # Code Parameters
+        self.reached_target_depth = False
         self.shutdown_triggered = False
         self.callback_frequency = 1e-5  # Frequency in seconds
 
-        # Overshooting counter
+        # Overshooting counterFalse
         self.overshoot_counter = 1
 
-        # Initialize bladder thrust
-        if self.isStrokeDownward: self.current_thrust = self.neutral_thrust + self.down_thrust
-        else: self.current_thrust = self.neutral_thrust + self.up_thrust
-        self.set_bladder(self.current_thrust)
+        self.current_depth = None
 
-        # Subscriptions and timers
+        # Subscription to Pressure
         self.subscription = self.create_subscription(
-            PointStamped,
-            f"/model/{glider}/sea_pressure_depth",
-            self.depth_callback,
+            FluidPressure,
+            f"/model/{glider}/sea_pressure",
+            self.pressure_callback,
             10
         )
+
         self.timer = self.create_timer(self.callback_frequency, self.process_depth)
 
-        self.get_logger().info(f"Initial bladder thrust set to {self.current_thrust}.")
+        # Initialize bladder thrust
+        if self.is_stroke_downward: self.current_thrust = self.neutral_thrust + self.down_thrust
+        else: self.current_thrust = self.neutral_thrust + self.up_thrust
+        self.set_bladder(self.current_thrust)
+        self.get_logger().info(f"Initial bladder set to {self.current_thrust}.")
 
     def set_bladder(self, bladder_value):
         """Send bladder command to Gazebo topic."""
@@ -49,57 +59,72 @@ class BladderMonitor(Node):
         self.get_logger().info(f"Bladder set to {bladder_value:.2f}.")
         self.current_thrust = bladder_value
 
-    def depth_callback(self, msg):
-        """Receive depth information."""
-        self.latest_depth_msg = msg.point.z
+    def pressure_callback(self, msg):
+        """Store the latest depth message."""
+        # Access the fluid pressure from the message
+        pressure = msg.fluid_pressure  # This is the pressure value in Pascals
+        # Convert pressure from Pascals to kPa
+        # Calculate depth from pressure
+        self.current_depth = (pressure - __std_pressure__) / __k__
 
-    def process_depth(self):
-        """Process depth and adjust bladder thrust."""
-        if not hasattr(self, 'latest_depth_msg') or self.latest_depth_msg is None:
-            return
-
-        current_depth = self.latest_depth_msg
-
-        if current_depth > self.target_depth and self.last_depth < self.target_depth:
+    def direct_thrust_control(self):
+        if self.current_depth > self.target_depth:
             self.get_logger().info(f"Target depth overshot. Slown down factor: {self.overshoot_counter}.")
             self.set_bladder(self.neutral_thrust + self.up_thrust/(2**self.overshoot_counter))
             self.overshoot_counter += 1
-        elif current_depth < self.target_depth and self.last_depth > self.target_depth:
+        else:
             self.get_logger().info(f"Target depth overshot. Slown down factor: {self.overshoot_counter}.")
             self.set_bladder(self.neutral_thrust + self.down_thrust/(2**self.overshoot_counter))
             self.overshoot_counter += 1
 
-        if self.last_depth is not None and self.last_time is not None:
-            # Equilibrium reached?
-            depth_error = self.target_depth - current_depth
+    def PID_loop(self):
+        return
 
-            # Stop bladder adjustment if target depth is reached and maintain neutral thrust
-            if abs(depth_error) < 1e-3:
-                self.get_logger().info(f"Target depth {self.target_depth} meters reached. Maintaining position.")
-                self.set_bladder(self.neutral_thrust)
-                self.has_reached_target_depth = True
-                self.shutdown_triggered = True
-                rclpy.shutdown()
+    def stop(self):
+        # Equilibrium reached?
+        depth_error = self.target_depth - self.current_depth
+
+        # Stop bladder adjustment if target depth is reached and maintain neutral thrust
+        if abs(depth_error) < 1e-3:
+            self.get_logger().info(f"Target depth {self.target_depth} meters reached. Maintaining position.")
+            self.set_bladder(self.neutral_thrust)
+            self.shutdown_triggered = True
+            shutdown()
+
+
+    def process_depth(self):
+        """Process depth and adjust bladder thrust."""
+        if not hasattr(self, 'current_depth') or self.current_depth is None:
+            return
+
+        # Has reached target depth?
+        if self.is_stroke_downward:
+            if not self.reached_target_depth and self.current_depth > self.target_depth:
+                self.reached_target_depth = True
+        else:
+            if not self.reached_target_depth and self.current_depth < self.target_depth:
+                self.reached_target_depth = True
+
+        if self.reached_target_depth:
+            self.direct_thrust_control()
+            self.stop()
 
         # Depth logger
-        self.get_logger().info(f"Current depth: {current_depth:.4f} meters")
-
-        # Update the previous depth and time for next iteration
-        self.last_depth = current_depth
+        self.get_logger().info(f"Current depth: {self.current_depth:.4f} meters")
 
 def main(args=None):
-    rclpy.init(args=args)
+    init(args=args)
 
-    depth_monitor = BladderMonitor()
+    bladder = BadderControlUnit()
 
     try:
-        rclpy.spin(depth_monitor)
+        spin(bladder)
     except KeyboardInterrupt:
-        depth_monitor.get_logger().info("Bladder monitoring interrupted by user.")
+        bladder.get_logger().info("Bladder monitoring interrupted by user.")
     finally:
-        if not depth_monitor.shutdown_triggered:
-            depth_monitor.destroy_node()
-            rclpy.shutdown()
+        if not bladder.shutdown_triggered:
+            bladder.destroy_node()
+            shutdown()
 
 if __name__ == '__main__':
     main()
